271893c7f9ef4a67c59de4d98e700e2b595d215d
kingbangkok4 <kingbangkok4@gmail.com>
2016-03-22 19:57:31 +0700

    Add assets

--- app/src/main/java/com/app/puzzlegame/MainActivity.java
+++ app/src/main/java/com/app/puzzlegame/MainActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class MainActivity  Activity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
