72bfe91c9a9ea884da4f790d97e25e7995c33af0
kingbangkok4 <kingbangkok4@gmail.com>
2016-03-22 19:52:53 +0700

    Add assets

--- app/src/main/java/com/app/puzzlegame/MainActivity.java
+++ app/src/main/java/com/app/puzzlegame/MainActivity.java
@@ -1,13 +1,25 @@
 package com.app.puzzlegame;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                    Intent i = new Intent(MainActivity.this, MenuActivity.class);
+                    startActivity(i);
+                finish();
+            }
+        }, 5000);
     }
 }
---
+++ app/src/main/java/com/app/puzzlegame/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.app.puzzlegame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nuttapong_i on 22/03/2559.
+ */
+public class MenuActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+    }
+}
