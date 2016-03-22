6791cb66fb0d6ed90f371d3ba0e64cc20aee3698
kingbangkok4 <kingbangkok4@gmail.com>
2016-03-22 19:56:44 +0700

    Add assets

--- app/src/main/java/com/app/puzzlegame/MainActivity.java
+++ app/src/main/java/com/app/puzzlegame/MainActivity.java
@@ -5,11 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class MainActivity extends Activity {
+public class MainActivity  Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
