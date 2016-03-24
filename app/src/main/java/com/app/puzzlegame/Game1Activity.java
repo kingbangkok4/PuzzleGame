92edfc19482cbae14cce89298881d949e2614af3
suchanat <suchanat2825@gmail.com>
2016-03-24 16:31:57 +0700

    Add game 1

---
+++ app/src/main/java/com/app/puzzlegame/Game1Activity.java
@@ -0,0 +1,15 @@
+package com.app.puzzlegame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by suchanat on 24/3/2559.
+ */
+public class Game1Activity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game1);
+    }
+}
