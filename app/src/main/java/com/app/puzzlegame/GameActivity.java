94337df9473374a03a4fafd0a6baea5a13231889
Administrator <kingbangkok4@gmail.com>
2016-03-29 01:07:44 +0700

    Add Games

--- app/src/main/java/com/app/puzzlegame/GameActivity.java
+++ app/src/main/java/com/app/puzzlegame/GameActivity.java
@@ -1,9 +1,12 @@
 package com.app.puzzlegame;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,7 +19,7 @@ import com.database.DatabaseActivity;
  */
 public class GameActivity extends Activity{
     private DatabaseActivity myDb = new DatabaseActivity(this);
-    private Button iQuestion, btAnswer1, btAnswer2, btAnswer3;
+    private Button iQuestion, btAnswer1, btAnswer2, btAnswer3, btExit;
     private ArrayList<HashMap<String, String>> gameList;
     private int i_random = 0;
 
@@ -29,27 +32,69 @@ public class GameActivity extends Activity{
         btAnswer1 = (Button)findViewById(R.id.btnAnswer1);
         btAnswer2 = (Button)findViewById(R.id.btnAnswer2);
         btAnswer3 = (Button)findViewById(R.id.btnAnswer3);
+        btExit = (Button)findViewById(R.id.btnExit);
 
         GamesAll();
 
         btAnswer1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                CheckAnswer("1");
             }
         });
         btAnswer2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                CheckAnswer("2");
             }
         });
         btAnswer3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                CheckAnswer("3");
             }
         });
+        btExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void CheckAnswer(String answer) {
+        switch (answer) {
+            case "1":
+                if(gameList.get(i_random).get("answer1").equals(gameList.get(i_random).get("answer"))){
+                    msgShow("ถูก");
+                    GamesAll();
+                }else{
+                    msgShow("ผิด");
+                }
+                break;
+            case "2":
+                if(gameList.get(i_random).get("answer2").equals(gameList.get(i_random).get("answer"))){
+                    msgShow("ถูก");
+                    GamesAll();
+                }else{
+                    msgShow("ผิด");
+                }
+                break;
+            case "3":
+                if(gameList.get(i_random).get("answer3").equals(gameList.get(i_random).get("answer"))){
+                    msgShow("ถูก");
+                    GamesAll();
+                }else{
+                    msgShow("ผิด");
+                }
+                break;
+            default:
+                break;
+        }
+
     }
 
     private void GamesAll() {
@@ -73,5 +118,9 @@ public class GameActivity extends Activity{
         Random r = new Random();
         int i1 = r.nextInt(max - min + 1) + min;
         return i1;
+    }
+
+    private void msgShow(String strMsg){
+        Toast.makeText(getApplicationContext(), strMsg, Toast.LENGTH_SHORT).show();
     }
 }
